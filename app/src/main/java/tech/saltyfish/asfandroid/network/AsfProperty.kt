61b64c00797354d74ad1a275df46e805b869b5f0
skaimid <zhuzhaoxin2000@outlook.com>
2020-03-18 09:59:23 +0800

    phrase json and use coroutines.

---
+++ app/src/main/java/tech/saltyfish/asfandroid/network/AsfProperty.kt
@@ -0,0 +1,91 @@
+package tech.saltyfish.asfandroid.network
+import com.squareup.moshi.JsonClass
+
+import com.squareup.moshi.Json
+
+
+@JsonClass(generateAdapter = true)
+data class AsfProperty(
+    @Json(name = "Message")
+    val message: String = "",
+    @Json(name = "Result")
+    val result: Result = Result(),
+    @Json(name = "Success")
+    val success: Boolean = false
+)
+
+@JsonClass(generateAdapter = true)
+data class Result(
+    @Json(name = "BuildVariant")
+    val buildVariant: String = "",
+    @Json(name = "GlobalConfig")
+    val globalConfig: GlobalConfig = GlobalConfig(),
+    @Json(name = "MemoryUsage")
+    val memoryUsage: Int = 0,
+    @Json(name = "ProcessStartTime")
+    val processStartTime: String = "",
+    @Json(name = "Version")
+    val version: String = ""
+)
+
+@JsonClass(generateAdapter = true)
+data class GlobalConfig(
+    @Json(name = "AutoRestart")
+    val autoRestart: Boolean = false,
+    @Json(name = "Blacklist")
+    val blacklist: List<Int> = listOf(),
+    @Json(name = "CommandPrefix")
+    val commandPrefix: String = "",
+    @Json(name = "ConfirmationsLimiterDelay")
+    val confirmationsLimiterDelay: Int = 0,
+    @Json(name = "ConnectionTimeout")
+    val connectionTimeout: Int = 0,
+    @Json(name = "CurrentCulture")
+    val currentCulture: String = "",
+    @Json(name = "Debug")
+    val debug: Boolean = false,
+    @Json(name = "FarmingDelay")
+    val farmingDelay: Int = 0,
+    @Json(name = "GiftsLimiterDelay")
+    val giftsLimiterDelay: Int = 0,
+    @Json(name = "Headless")
+    val headless: Boolean = false,
+    @Json(name = "IPC")
+    val iPC: Boolean = false,
+    @Json(name = "IPCPassword")
+    val iPCPassword: String = "",
+    @Json(name = "IdleFarmingPeriod")
+    val idleFarmingPeriod: Int = 0,
+    @Json(name = "InventoryLimiterDelay")
+    val inventoryLimiterDelay: Int = 0,
+    @Json(name = "LoginLimiterDelay")
+    val loginLimiterDelay: Int = 0,
+    @Json(name = "MaxFarmingTime")
+    val maxFarmingTime: Int = 0,
+    @Json(name = "MaxTradeHoldDuration")
+    val maxTradeHoldDuration: Int = 0,
+    @Json(name = "OptimizationMode")
+    val optimizationMode: Int = 0,
+    @Json(name = "s_SteamOwnerID")
+    val sSteamOwnerID: String = "",
+    @Json(name = "Statistics")
+    val statistics: Boolean = false,
+    @Json(name = "SteamMessagePrefix")
+    val steamMessagePrefix: String = "",
+    @Json(name = "SteamOwnerID")
+    val steamOwnerID: Long = 0,
+    @Json(name = "SteamProtocols")
+    val steamProtocols: Int = 0,
+    @Json(name = "UpdateChannel")
+    val updateChannel: Int = 0,
+    @Json(name = "UpdatePeriod")
+    val updatePeriod: Int = 0,
+    @Json(name = "WebLimiterDelay")
+    val webLimiterDelay: Int = 0,
+    @Json(name = "WebProxy")
+    val webProxy: String = "",
+    @Json(name = "WebProxyPassword")
+    val webProxyPassword: String = "",
+    @Json(name = "WebProxyUsername")
+    val webProxyUsername: String = ""
+)
--- app/src/main/java/tech/saltyfish/asfandroid/overview/OverviewViewModel.kt
+++ app/src/main/java/tech/saltyfish/asfandroid/overview/OverviewViewModel.kt
@@ -4,38 +4,55 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.http.Header
 import tech.saltyfish.asfandroid.network.AsfApi
+import tech.saltyfish.asfandroid.network.AsfProperty
+import java.lang.Exception
 
 class OverviewViewModel : ViewModel() {
 
     private val _system = MutableLiveData<String>()
-    val system:LiveData<String>
-        get()  = _system
+    val system: LiveData<String>
+        get() = _system
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(
+        viewModelJob + Dispatchers.Main
+    )
 
     init {
-        _system.value = ""
+        _system.value = "-"
 
         getSystemInfo()
     }
 
-    private fun getSystemInfo(){
-        AsfApi.retrofitService.getProperties("zzx20001223").enqueue(
-            object: Callback<String>{
-                override fun onFailure(call: Call<String>, t: Throwable) {
-                    _system.value = "Failure: " + t.message
+    private fun getSystemInfo() {
+        coroutineScope.launch {
+
+
+            var getAsfPropertyDeferred = AsfApi.retrofitService.getProperties("zzx20001223")
+//            try {
+//                // Await the completion of our Retrofit request
+//                var listResult = getPropertiesDeferred.await()
+//                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+//            } catch (e: Exception) {
+//                _response.value = "Failure: ${e.message}"
+//            }
+                try {
+                    var rs = getAsfPropertyDeferred.await()
+                    _system.value = rs.result.globalConfig.iPC.toString()
+                }catch (e:Exception){
+                    _system.value = e.message
                 }
 
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _system.value = response.body()
-                    Log.d("rs",system.value)
-                }
-            }
+        }
 
-        )
     }
 
 }
